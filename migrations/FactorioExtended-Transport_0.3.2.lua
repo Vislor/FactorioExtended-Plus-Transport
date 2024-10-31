644c33979f4e70268b286e439d57221e903e9004
Vislor <wjerla3@verizon.net>
2024-10-31 17:50:23 -0400

    dev: Initial 2.0 commit

--- migrations/FactorioExtended-Transport_0.3.2.lua
+++
@@ -1,9 +0,0 @@
-for index, force in pairs(game.forces) do
-  local technologies = force.technologies
-  local recipes = force.recipes
-
-  if technologies["logistics-3"].researched then
-    technologies["railway-2"].researched = true
-    technologies["railway-3"].researched = true
-  end
-end
--- migrations/FactorioExtended-Transport_0.4.1.lua
+++
@@ -1,9 +0,0 @@
-for index, force in pairs(game.forces) do
-    local technologies = force.technologies
-    local recipes = force.recipes
-
-    if technologies["factorio-extended-fluid-handling"].researched then
-        recipes["offshore-pump-mk2"].enabled = true
-        recipes["offshore-pump-mk2"].reload()
-    end
-end
